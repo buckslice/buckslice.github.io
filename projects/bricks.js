89050d19ffe853785153a338d828a2de9d06f7cf
jfc3 <buckslice@gmail.com>
2019-03-16 04:07:42 -0700

    making some NICE improvements

--- projects/bricks.js
+++ projects/bricks.js
@@ -5,8 +5,15 @@ let xbricks, ybricks, xslots, brickW, brickH, grav;
 var canvas;
 
 let brickCount = 0;
-let brokeOwn = false;
 let nextBreakCounter = 0;
+let brokeOwn = true;
+
+if(typeof lost == 'undefined'){
+	lost = false;
+}
+if(typeof lab == 'undefined'){
+	lab = false;
+}
 
 function setup(){
 	canvas = createCanvas(windowWidth, windowHeight);
@@ -23,6 +30,20 @@ function setup(){
 	brickW = windowWidth / xbricks;
 	brickH = windowHeight / ybricks;
 	
+	dposx = random(0, windowWidth-durw-20);
+	dposy = random(windowHeight / 2, windowHeight-durh);
+	
+	if(lost){
+		brokeOwn = false;
+	}
+	
+	// make it so clicking on links doesnt break bricks
+	links = selectAll('.link');
+	for(let i = 0; i < links.length; ++i){
+		links[i].mouseOver(mouseOverLink);
+		links[i].mouseOut(mouseOutLink);
+	}
+	
 	for (let y = 0; y < ybricks; ++y){
 		let row = [];
 		let oddY = y % 2 == 1;
@@ -34,9 +55,10 @@ function setup(){
 			let r = yp * 0.5-g;
 			let b = xp * 0.4+0.1-g*0.6;
 			g = g * 0;
-			
-			r = b;
-			g = b;
+			if(!lab){
+				r = b;
+				g = b;
+			}
 			
 			if ((x+y) % 2 == 1){
 				row.push(new Brick((x-1)*brickW/2, y * brickH, createVector(r,g,b)));
@@ -51,14 +73,79 @@ function setup(){
 	
 }
 
+let canBreak = true;
+function mouseOverLink(){
+	canBreak = false;
+}
+function mouseOutLink(){
+	canBreak = true;
+}
+
 function windowResized(){
 	resizeCanvas(windowWidth, windowHeight);
+}
+
+// make sure u clicking on the dur correctly
+let good = false;
+let opening = false;
+let openTime = 0;
+let dposx = 0;
+let dposy = 0;
+let durw = 50;
+let durh = 80;
+function mousePressed(){
+	good = checkDur();
+}
+function mouseReleased(){
+	if(good && checkDur()){
+		if(!opening){
+			openTime = millis();
+		}
+		opening = true;
+		setTimeout(function(){window.location.href = "laboratory.html";}, 500);
+	}
+}
+// check if color at point is not monochromatic (dur)
+function checkCol(x,y){
+	c = get(x,y);
+	return c[0] != c[1] || c[0] != c[2];
+}
+function checkDur(){
+	return !lost && !lab && 
+	checkCol(mouseX,mouseY) &&
+	checkCol(dposx, dposy) &&
+	checkCol(dposx+durw, dposy) &&
+	checkCol(dposx, dposy+durh) &&
+	checkCol(dposx+durw, dposy+durh);
 }
 
 function draw(){
 	background(0,0,0);
 	
-	if (mouseIsPressed && mouseButton == LEFT){	
+	// nothing to see here...
+	if(!lab && !lost){
+		if(opening){
+			fill(0,0,0);
+			rect(dposx, dposy, durw, durh);
+			for(let i = 0; i < 5; ++i){
+				let r = i*i * 0.04 + 0.1;
+				let b = 0.3 - i * .05;
+				stroke(r*0.5, 0, b*0.5);
+				fill(r, 0, b);
+				rect(dposx + i * 5, dposy + i * (12 - i * 1), durw - i * 8, durh - i * 2 -  i * (12 - i * 1));
+			}
+		}
+		stroke(.15,.1,.05);
+		fill(.22,.17,.07);
+		let rf = opening ? max(.1, 1.0 - (millis() - openTime)/500) : 1.0;
+		for(let i = 0; i < 4; ++i){
+			rect(dposx + (i * durw / 4 * rf), dposy, durw / 4 * rf, durh);		
+		}
+		fill(.15,.1,.05);
+		rect(dposx + (durw-10)*rf, dposy + durh/2, 10, 5);
+	}
+	
+	if (canBreak && mouseIsPressed && mouseButton == LEFT){	
 		if(breakBrick(mouseX, mouseY)){
 			brokeOwn = true;
 		}
@@ -77,6 +164,8 @@ function draw(){
 			}
 		}
 	}
+	
+	mouseOverLink = false;
 }
 
 // break brick at xpos and ypos on screen
