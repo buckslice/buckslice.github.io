85c6335c9dec1b4b736aedb997a558ba0873cea9
jfc3 <buckslice@gmail.com>
2020-01-24 08:38:50 -0800

    got filters working good and saving correctly upon page navigation

--- libs/myfilterer.js
+++ libs/myfilterer.js
@@ -2,6 +2,7 @@
 var filters = [];
 var cards = document.getElementsByClassName("card");
 var btnContainer = document.getElementById("myBtnContainer");
+var btnGroups = btnContainer.getElementsByClassName("btnGroup");
 var buttons = btnContainer.getElementsByClassName("btn");
 // for (var i = 0; i < buttons.length; i++) {
 //     buttons[i].addEventListener("click", function () {
@@ -9,15 +10,81 @@ var buttons = btnContainer.getElementsByClassName("btn");
 //     });
 // }
 
-showAll();
+loadFromHash();
 
-function setFilter(f, b) { // adds filter or removes it if its already there
-    let i = filters.indexOf(f);
+function disableOtherActives(b) {
+    if (b.parentNode.classList.contains("btnGroup")) {
+        let nodes = b.parentNode.childNodes;
+        for (let i = 0; i < nodes.length; ++i) {
+            if (nodes[i].tagName == "BUTTON" && nodes[i] != b) {
+                if (nodes[i].classList.contains("active")) {
+                    let j = filters.indexOf(nodes[i].getAttribute("data-tag"));
+                    if (j > -1) {
+                        filters.splice(j, 1);
+                    }
+                    nodes[i].classList.remove("active");
+                }
+            }
+        }
+    }
+}
+
+function shouldCardBeShown(card) {
+    if (!("tag" in card.dataset)) {
+        return false;
+    }
+    let tags = card.getAttribute("data-tag").split(" ");
+    for (let i = 0; i < filters.length; ++i) {
+        if (tags.indexOf(filters[i]) == -1) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function loadFromHash(){
+    if(location.hash){
+        let hf = location.hash.substring(1).split("_");
+        console.log(hf);
+        for(let i = 0; i < buttons.length; ++i){
+            if(hf.indexOf(buttons[i].getAttribute("data-tag")) > -1){
+                setFilter(buttons[i]);
+            }
+        }
+    }else{
+        showAll();
+    }
+}
+
+function setHash() {
+    let newhash = "#";
+    for (let i = 0; i < filters.length; ++i) {
+        newhash += filters[i];
+        if (i < filters.length - 1) {
+            newhash += "_";
+        }
+    }
+    if (newhash == "#") {
+        newhash = " ";
+    }
+    history.replaceState(undefined, undefined, newhash);
+}
+
+function setFilter(b) { // adds filter or removes it if its already there
+
     b.classList.toggle("active");
-    if (i > -1) {
+
+    let f = b.getAttribute("data-tag");
+    let i = filters.indexOf(f);
+    if (i > -1) { // if filter is in, then toggle it off
         filters.splice(i, 1);
     } else {
         filters.push(f);
+
+        // since turning on filter check if another active in this group and disable it (this is basically manual scuffed radio buttons)
+        //console.log(b.parentNode.childNodes);
+        disableOtherActives(b);
+
     }
     if (filters.length == 0) {
         showAll();
@@ -27,18 +94,12 @@ function setFilter(f, b) { // adds filter or removes it if its already there
             buttons[0].classList.remove("active");
         }
     }
+
+    setHash();
+
     // recalc filters (operating in AND mode)
     for (let i = 0; i < cards.length; ++i) {
-        let shouldShow = false;
-        for (let j = 0; j < filters.length; ++j) {
-            if (!cards[i].classList.contains(filters[j])) {
-                //shouldShow = true;
-                break;
-            }
-            if (j == filters.length - 1) { // comment this if and undo the ! for other if and uncomment line to make OR
-                shouldShow = true;
-            }
-        }
+        let shouldShow = shouldCardBeShown(cards[i]);
         let isShowing = cards[i].classList.contains("show")
         if (shouldShow && !isShowing) {
             cards[i].classList.add("show");
@@ -65,5 +126,7 @@ function showAll() {
             cards[i].classList.add("show");
         }
     }
+
+    setHash();
 
 }
